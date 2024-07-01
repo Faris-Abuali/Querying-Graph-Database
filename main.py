c8784e40f9b1dc46246c7d59330162590a3c856c
Faris Abu Ali <faris.abuali@edu.rptu.de>
2024-07-01 18:38:45 +0200

    refactor(graph): add type annotations for Graph class

--- main.py
+++ main.py
@@ -2,23 +2,25 @@
 
 import json
 import z3
+from typing import TypeAlias
 
+Node: TypeAlias = str
 
 class Graph:
     def __init__(self):
-        self.nodes = set()
-        self.adjacency_map = {}
+        self.nodes: set[Node] = set()
+        self.adjacency_map: dict[Node, list[Node]] = {}
 
-    def add_node(self, node):
+    def add_node(self, node: Node):
         self.nodes.add(node)
         if node not in self.adjacency_map:
             self.adjacency_map[node] = []
 
-    def add_edge(self, from_node, to_node):
+    def add_edge(self, from_node: Node, to_node: Node):
         self.add_node(from_node)
         self.add_node(to_node)
         self.adjacency_map[from_node].append(to_node)
-
+        
     def __str__(self):
         result = "Graph:\n"
         for node in sorted(self.nodes):
